2c355a2ebe8b568f46c62544c19c03cc43ff7210
Jessan23 <jessicaanairayagrez@gmail.com>
2024-12-02 16:30:04 +0000

    entrega proyecto SWDM

--- src/models.py
+++ src/models.py
@@ -7,23 +7,13 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+#class Person(Base):
+   # __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    #id = Column(Integer, primary_key=True)
+    #name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
 class Usuario(Base):
     __tablename__ = 'usuario'
@@ -35,26 +25,44 @@ class Usuario(Base):
     email = Column(String(250), nullable=False)
     password = Column(String(30), nullable=False)
     subscripcionDate = (Column(DateTime))
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    name= Column (String(100), nullable=False)
+    climate = Column (String(100))
+    cleated= Column (String(100))
+
 
 class Caract(Base):
-    _tablename_ = 'caract'
+    __tablename__ = 'caract'
     id = Column(Integer, primary_key=True)
     gender = Column(String(100))
     hair_color = Column(String(100))
     name = Column (String(100), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    
 
-class Planets(Base):
-    _tablename_ = 'planets'
+class Nave(Base):
+    __tablename__ = 'nave'
     id = Column(Integer, primary_key=True)
-    climate= Column (String(100))
     name= Column (String(100), nullable=False)
-    population = (Integer)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    model = Column (String(100))
+    consumables= Column (String(100))
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    Usuario_id =Column (Integer, ForeignKey('usuario.id'))
+    usuario = relationship (Usuario)
+    Planetas_id = Column (Integer, ForeignKey('planetas.id'))
+    planetas = relationship (Planetas)
+    Caract_id = Column(Integer, ForeignKey('caract.id'))
+    caract = relationship(Caract)
+    Nave_id = Column (Integer, ForeignKey('nave.id'))
+    nave = relationship (Nave)
+
+
 
 
     def to_dict(self):
@@ -62,3 +70,4 @@ class Planets(Base):
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+ 
